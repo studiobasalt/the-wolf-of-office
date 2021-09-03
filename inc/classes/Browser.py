00d718ce33d4f188af7c4c032e012fa14877058d
Derrin616 <mail1@derrin.nl>
2021-09-03 15:23:08 +0200

    Bugfix Browser class

--- inc/classes/Browser.py
+++ inc/classes/Browser.py
@@ -5,7 +5,7 @@ class Browser():
     def goToUrl(self, url):
         if(platform.system() == "Linux"):
             os.system("pkill -o chromium-")
-            os.system("-u pi DISPLAY=:0.0 chromium-browser --kiosk "+ str(url) +" &")
+            os.system("sudo -u pi DISPLAY=:0.0 chromium-browser --kiosk "+ str(url) +" &")
         elif(platform.system() == "Darwin"):
             os.system("pkill -o Chromium")
             os.system("DISPLAY=:0.0 chromium --kiosk "+ str(url) +" &")
