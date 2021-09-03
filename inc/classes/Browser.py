99eb3d0894e9bb4d80e3d64385718e4bdd1c7950
Derrin616 <mail1@derrin.nl>
2021-09-03 13:33:30 +0200

    Added browser class

---
+++ inc/classes/Browser.py
@@ -0,0 +1,13 @@
+import os, platform
+
+class Browser():
+
+    def goToUrl(self, url):
+        if(platform.system() == "Linux"):
+            os.system("pkill -o chromium-")
+            os.system("DISPLAY=:0.0 chromium-browser --kiosk "+ str(url) +" &")
+        elif(platform.system() == "Darwin"):
+            os.system("pkill -o Chromium")
+            os.system("DISPLAY=:0.0 chromium --kiosk "+ str(url) +" &")
+
+help=Browser()
