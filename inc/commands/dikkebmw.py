2cfdc6a1721bd448b78b2241dcc7c6f9c48bf8fc
Derrin616 <derrin@beekman.eu>
2021-12-08 14:53:24 +0100

    [dikke_bmw] added sound

---
+++ inc/commands/dikkebmw.py
@@ -0,0 +1,11 @@
+from inc.classes.Command import Command
+from inc.classes.PlaySound import PlaySound
+
+class myCommand(Command):
+
+    COMMAND = '/dikkebmw'
+    DESCRIPTION = 'DIKKE BMW'
+
+    def run(self, data):
+        self.bot.sendMessage(data['chat_id'], "DIKKE BMW")
+        PlaySound().play('dikkebmwsound.py')
