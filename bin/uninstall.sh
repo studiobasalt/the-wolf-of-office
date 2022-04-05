cfbb45655eb4f46f9a49cc8d8ae00efad35dc492
matthijs knigge <Matthijs.knigge@gmail.com>
2022-04-06 00:00:49 +0200

    Update uninstall.sh

--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -2,16 +2,19 @@
 
 cd /usr/bin/the-wolf-of-office
 
-pip3 uninstall importlib
-pip3 uninstall pygame
-pip3 uninstall tinydb
-pip3 uninstall flask
-pip3 uninstall python-dotenv
+echo '-- Start Uninstall --'
+
+pip3 uninstall importlib -y
+pip3 uninstall pygame -y
+pip3 uninstall tinydb -y
+pip3 uninstall flask -y
+pip3 uninstall python-dotenv -y
 apt purge -y raspberrypi-ui-mods chromium-browser
 apt purge -y unclutter
 mv ./res/teleport.yaml /etc/
-apt-get purge realvnc-vnc-server
+apt purge realvnc-vnc-server -y
 raspi-config nonint do_vnc 1
+rm /usr/share/novnc/index.html
 apt purge novnc -y
 rm /home/pi/.config/lxsession/LXDE-pi/autostart
 
