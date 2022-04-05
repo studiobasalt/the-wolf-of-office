e133932f524fd16b02f91edc72ecbfc4b6f1939c
matthijs knigge <Matthijs.knigge@gmail.com>
2022-04-06 00:00:41 +0200

    Update setScreenOrientation.sh

--- bin/inc/system/setScreenOrientation.sh
+++ bin/inc/system/setScreenOrientation.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd
-
-. ./usr/bin/the-wolf-of-office/bin/inc/utls/load-env-file.sh
+. /usr/bin/the-wolf-of-office/bin/inc/utls/load-env-file.sh
 
 #right left normal inverted
 DISPLAY=:0 xrandr --output HDMI-1 --rotate $SCREEN_ORIENTATION
