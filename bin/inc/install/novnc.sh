202360426f89a2efde30be152a2566624b26cff8
matthijs knigge <Matthijs.knigge@gmail.com>
2022-04-05 20:19:07 +0200

    [VNC] Better setup

--- bin/inc/install/novnc.sh
+++ bin/inc/install/novnc.sh
@@ -9,3 +9,13 @@ sudo raspi-config nonint do_vnc 0
 # Setup the client server
 cd /usr/local/share/
 sudo git clone https://github.com/kanaka/noVNC
+
+# Set VNC password
+vncpasswd -service
+
+# Install config
+cd /usr/bin/the-wolf-of-office
+cp res/vncserver-x11.config /etc/vnc/config.d/vncserver-x11
+
+# Restart service
+service vncserver-x11-serviced restart
