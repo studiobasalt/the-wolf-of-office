9ca08f43e297617ebefe1225d5b23e339908cc53
matthijs knigge <Matthijs.knigge@gmail.com>
2022-04-06 00:00:45 +0200

    Update update.sh

--- bin/inc/system/update.sh
+++ bin/inc/system/update.sh
@@ -6,6 +6,6 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 sudo apt -y full-upgrade
-sudo apt autoclean
-sudo apt-get clean
-sudo apt autoremove
+sudo apt autoclean -y
+sudo apt-get clean -y
+sudo apt autoremove -y
