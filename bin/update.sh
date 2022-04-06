48c6b35f9d148521411a2458b2592427dcc40c09
matthijs knigge <Matthijs.knigge@gmail.com>
2022-04-06 15:47:09 +0200

    Update update.sh

--- bin/update.sh
+++ bin/update.sh
@@ -6,7 +6,6 @@ bash ./inc/update-the-wolf.sh
 bash ./boot.sh
 bash ./inc/system/set-startup-script.sh
 bash ./inc/install/env-file.sh update
-bash ./inc/system/update.sh
 bash ./inc/install/deps.sh
 bash ./inc/install/services.sh
 bash ./inc/utls/restart-services.sh
