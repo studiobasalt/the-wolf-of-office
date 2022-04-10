f6fba2edf219b5a3c22bcbd3c7e52ed0464fa415
matthijs knigge <Matthijs.knigge@gmail.com>
2022-04-10 16:56:51 +0200

    [Dependies] Faster pip install & add xscreensaver

--- bin/inc/install/deps.sh
+++ bin/inc/install/deps.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 echo '-- Install pip (python3) libs --'
+
+# Cache pip list
+pipList=$(pip3 list --disable-pip-version-check)
+
+# Fast install and check
 function install {
-    out=$(pip3 list --disable-pip-version-check | grep $1)
+    out=$(echo "$pipList" | grep $1)
     if [[ -z $out ]]; then
         pip3 install $1
         return
@@ -10,6 +15,7 @@ function install {
     echo "$1 already intalled"
 }
 
+# Install pip3 libs
 install importlib
 install pygame
 install tinydb
@@ -17,5 +23,4 @@ install Flask
 install python-dotenv
 
 echo '-- Install os packages --'
-apt install -y raspberrypi-ui-mods chromium-browser
-apt install -y unclutter
+apt install -y raspberrypi-ui-mods chromium-browser unclutter xscreensaver
