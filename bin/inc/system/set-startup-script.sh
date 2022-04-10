d97607bba0af905ee45f3bd35c6f1ae0b4977a97
matthijs knigge <Matthijs.knigge@gmail.com>
2022-04-10 16:56:15 +0200

    [Startup script] Set owner of file to pi

--- bin/inc/system/set-startup-script.sh
+++ bin/inc/system/set-startup-script.sh
@@ -5,3 +5,5 @@ echo "-- Set startup script --"
 mkdir -p /home/pi/.config/lxsession/LXDE-pi/
 
 cp /usr/bin/the-wolf-of-office/res/lxsession-autostart /home/pi/.config/lxsession/LXDE-pi/autostart
+
+chown pi:pi /home/pi/.config/lxsession/LXDE-pi/autostart
