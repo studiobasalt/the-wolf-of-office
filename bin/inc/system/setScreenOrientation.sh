602a52a8d50579a5f7d8fa961184eb53e2716685
matthijs knigge <Matthijs.knigge@gmail.com>
2022-04-17 11:58:05 +0200

    [System] Screen screen orientation fix

--- bin/inc/system/setScreenOrientation.sh
+++ bin/inc/system/setScreenOrientation.sh
@@ -4,5 +4,5 @@ echo '-- Set screen orientation --'
 
 . /usr/bin/the-wolf-of-office/bin/inc/utls/load-env-file.sh
 
-#right left normal inverted
-DISPLAY=:0 xrandr --output HDMI-1 --rotate $SCREEN_ORIENTATION
+# #right left normal inverted
+sudo ORIENTATION=$SCREEN_ORIENTATION -H -u pi bash -c 'DISPLAY=:0 xrandr --output HDMI-1 --rotate $ORIENTATION'
