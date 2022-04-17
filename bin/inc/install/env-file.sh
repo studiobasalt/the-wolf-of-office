8197ad2bc40a2b0b3a871d48f2fb891b0e6ec244
matthijs knigge <Matthijs.knigge@gmail.com>
2022-04-17 19:48:31 +0200

    [System install] Env new warning

--- bin/inc/install/env-file.sh
+++ bin/inc/install/env-file.sh
@@ -5,6 +5,7 @@ cd /usr/bin/the-wolf-of-office/
 if [[ $1 != 'update' ]]; then
     # Get user inputs
     echo Input device name, this is also the hostname for the pi
+    echo "!! No caps in name (this is used for hostname and teleport novnc server)"
     read DEVICE_NAME
     echo Set orientation of the screen on bootup: right left normal inverted
     echo This can you edit inside /usr/bin/the-wolf-of-office/.env
