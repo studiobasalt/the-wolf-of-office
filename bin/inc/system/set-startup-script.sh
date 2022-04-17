060bfe0a209e5f780e39f471b517cf959fb1fc53
matthijs knigge <Matthijs.knigge@gmail.com>
2022-04-17 15:13:21 +0200

    [Startup script] Update install & lxsession bug fixes

--- bin/inc/system/set-startup-script.sh
+++ bin/inc/system/set-startup-script.sh
@@ -2,8 +2,15 @@
 
 echo "-- Set startup script --"
 
+# Set chromium behafior
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+# Create script dir
 mkdir -p /home/pi/.config/lxsession/LXDE-pi/
 
+# Copy template to user boot dir
 cp /usr/bin/the-wolf-of-office/res/lxsession-autostart /home/pi/.config/lxsession/LXDE-pi/autostart
 
+# Set ownership
 chown pi:pi /home/pi/.config/lxsession/LXDE-pi/autostart
