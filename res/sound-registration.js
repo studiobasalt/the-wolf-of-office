ebaea63c7feab42368a1ded294b636c8f8437544
matthijs knigge <Matthijs.knigge@gmail.com>
2022-06-25 22:39:57 +0200

    [Soundbaord interactions] INIT

--- res/sound-registration.js
+++ res/sound-registration.js
@@ -2,25 +2,25 @@ export default [
     {
         name: "biem",
         accessControlProperty: "basicsound",
-        file: "res/sounds/biem.mp3",
+        file: "biem.mp3",
         description: "Biem !!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     },
     {
         name: "Sales Bell",
         accessControlProperty: "basicsound",
-        file: "res/sounds/bell.mp3",
+        file: "bell.mp3",
         description: "New York stock exchange bell"
     },
     {
         name: "flipflap",
         accessControlProperty: "basicsound",
-        file: "res/sounds/flipflap.mp3",
+        file: "flipflap.mp3",
         description: "OG kantoor sound"
     },
     {
         name: "lunch",
         accessControlProperty: "basicsound",
-        file: "res/sounds/Lunch.mp3",
+        file: "Lunch.mp3",
         description: "We gaan lunchen geluid"
     }
 ]
