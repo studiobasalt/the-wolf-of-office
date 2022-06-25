3c1a9a07fb196f33e47b26a4ebb5376a530b459d
matthijs knigge <Matthijs.knigge@gmail.com>
2022-06-25 19:03:06 +0200

    Update template.js

--- apps/command-server/commands/template.js
+++ apps/command-server/commands/template.js
@@ -2,13 +2,18 @@ import Base from './base.js'
 
 class Template extends Base{
     constructor(){
+        super()
         this.name = 'template'
         this.capabilitiesLevel = 5
         this.description = "Template to see how you can build a command"
     }
 
-    run(){
+    async run(){
         this.play('bell.mp3') // Play a sound in de res/sounds folde
         this.context // Get the context of all commands
+        await this.say('Hello world', blocks, sendHidden) // Send a message in the channel
+        await this.openView(blocks) // Open a view in the channel
     }
 }
+
+export default Template
