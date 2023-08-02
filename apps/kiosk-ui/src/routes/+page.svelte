f792af00d25b3916e5ef96329bedb367be1f0dad
Matthijs Knigge <matthijs@volcano.nl>
2023-08-02 15:58:47 +0200

    Update +page.svelte

--- apps/kiosk-ui/src/routes/+page.svelte
+++ apps/kiosk-ui/src/routes/+page.svelte
@@ -1,33 +1,43 @@
 <script>
-  import Logo from '$lib/logo.svelte';
-  import { onMount } from 'svelte';
-  import { goto } from '$app/navigation';
-  import CenterContainer from '$lib/centerContainer.svelte';
+    import Logo from '$lib/logo.svelte';
+    import { onMount, onDestroy } from 'svelte';
+    import { goto } from '$app/navigation';
+    import CenterContainer from '$lib/centerContainer.svelte';
+    import Footer from '@lib/footer.svelte';
+    import { defaultDeviceStore } from '@stores/local';
 
-  // Count down to take redirect to next page
-  let timeLeft = 5;
-  onMount(() => {
-    const interval = setInterval(() => {
-      timeLeft -= 1;
-      if (timeLeft === 0) {
-        clearInterval(interval);
-      }
-    }, 1000);
-  });
+    // Count down to take redirect to next page
+    let timeLeft = 10;
+    let timer;
+    onMount(() => {
+        timer = setInterval(() => {
+            timeLeft -= 1;
+            if (timeLeft === 0) {
+                goto('/playmode');
+                clearInterval(timer);
+            }
+        }, 1000);
+    });
+
+    onDestroy(() => {
+        clearInterval(timer);
+    });
 </script>
 
 <CenterContainer>
-  <Logo />
+    <Logo />
 
-  <h1>Welcome to the Wolf of Office</h1>
+    <h1 style="text-align: center">Welcome to the Wolf of Office</h1>
 
-  {#if false}
-    <h3>
-      kiosk screen starts in {timeLeft} seconds
-    </h3>
-  {:else}
-    <h3>No kiosk screen found, please contact your administrator</h3>
-  {/if}
+    {#if $defaultDeviceStore}
+        <h3>
+            kiosk screen starts in {timeLeft} seconds
+        </h3>
+    {:else}
+        <h3>No device is set, open device management to set a device</h3>
+    {/if}
 
-  <button on:click={() => goto('/dashboard')}> Open Dashboard </button>
+    <button on:click={() => goto('/dashboard')}> Open Dashboard </button>
 </CenterContainer>
+
+<Footer />
