a8d89d0998f3bfdffac3c176e6a3fb246bf1782c
Matthijs Knigge <matthijs@volcano.nl>
2023-08-03 17:15:48 +0200

    feat(setup): Include kiosk url also in the env for after the update

--- apps/pi-setup/inc/install/env-file.sh
+++ apps/pi-setup/inc/install/env-file.sh
@@ -14,6 +14,9 @@ if [[ $1 != 'update' ]]; then
     read SCREEN_ORIENTATION
     echo Input the main user name of the pi
     read USER_NAME
+    echo 'Input the kiosk url for the app without (http:// or https://)'
+    read KIOSK_URL
+    KIOSK_URL=$(printf '%s\n' "$KIOSK_URL" | sed -e 's/[\/&]/\\&/g')
 else
     # Load old env file
     . ./inc/load-env-file.sh
@@ -26,14 +29,10 @@ cp template.env .env
 sed -e 's/DEVICE_NAME_INPUT/'$DEVICE_NAME'/' ./.env -i
 sed -e 's/SCREEN_ORIENTATION_INPUT/'$SCREEN_ORIENTATION'/' ./.env -i
 sed -e 's/USER_NAME_INPUT/'$USER_NAME'/' ./.env -i
+sed -e 's/KIOSK_URL_INPUT/'$KIOSK_URL'/' ./.env -i
 
 
 echo '-- .ENV Setup for kiosk browser --'
 cd /usr/bin/the-wolf-of-office/apps/pi-setup/assets/
-if [[ $1 != 'update' ]]; then
-    echo 'Input the kiosk url for the app without (http:// or https://)'
-    read KIOSK_URL
-fi
 cp lxsession-autostart.template lxsession-autostart
-KIOSK_URL=$(printf '%s\n' "$KIOSK_URL" | sed -e 's/[\/&]/\\&/g')
 sed -e 's/KIOSK_URL_INPUT/'$KIOSK_URL'/' ./lxsession-autostart -i
