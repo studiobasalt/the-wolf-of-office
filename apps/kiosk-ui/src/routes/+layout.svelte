badb7b6e340a54e13d5252778a5de9809cf1e024
Matthijs Knigge <matthijs@volcano.nl>
2023-08-14 21:30:48 +0200

    fix(ui): Init stores also after an login

--- apps/kiosk-ui/src/routes/+layout.svelte
+++ apps/kiosk-ui/src/routes/+layout.svelte
@@ -5,18 +5,18 @@
     import { deviceStoreInit } from '@stores/device';
     import { viewStoreInit } from '@stores/view';
     import { globalsStoreInit } from '@stores/globals';
-    import { onMount } from 'svelte';
-
-    onMount(() => {
-        deviceStoreInit();
-        viewStoreInit();
-        globalsStoreInit();
-    });
-
+    
     auth.onAuthStateChanged((user) => {
         if (!browser) return;
-        if (user) return;
+        if (user) {
+            deviceStoreInit();
+            viewStoreInit();
+            globalsStoreInit();
+            return
+        };
         if (window?.location.pathname === '/auth') return;
+
+        // Redirect to auth
         goto('/auth');
     });
 </script>
